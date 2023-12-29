97fcf184a0c57a3b4552e220cbbe38a33f721e1e
Danilo Pezo <danilo_97pem@hotmail.com>
2023-12-29 18:56:17 -0300

    Implementando a requisicao DELETE

--- src/main/java/agendamento/med/api/controller/MedicoController.java
+++ src/main/java/agendamento/med/api/controller/MedicoController.java
@@ -34,9 +34,9 @@ public class MedicoController {
         medico.atualizarInformacoes(dados);
     }
 
-   // @DeleteMapping("/{id}")
-   // @Transactional
-   // public void excluir(@PathVariable Long id) {
-   //     repository.deleteById(id);
-   // }
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
