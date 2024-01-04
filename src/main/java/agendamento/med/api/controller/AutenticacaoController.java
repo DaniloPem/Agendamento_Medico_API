7a6eecd840d3aedb0677ec855cceaaf375fcc996
Danilo Pezo <danilo_97pem@hotmail.com>
2024-01-04 17:30:02 -0300

    Implementamos controller de autenticação

---
+++ src/main/java/agendamento/med/api/controller/AutenticacaoController.java
@@ -0,0 +1,24 @@
+package agendamento.med.api.controller;
+
+import agendamento.med.api.domain.usuario.DadosAutenticacao;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+
+    }
+}
