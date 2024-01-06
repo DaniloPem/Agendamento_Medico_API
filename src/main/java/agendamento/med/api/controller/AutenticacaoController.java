1308a3b647c85fbf51dc71999e7baea06728dbce
Danilo Pezo <danilo_97pem@hotmail.com>
2024-01-06 12:43:59 -0300

    Geramos tokens JWT

--- src/main/java/agendamento/med/api/controller/AutenticacaoController.java
+++ src/main/java/agendamento/med/api/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package agendamento.med.api.controller;
 
 import agendamento.med.api.domain.usuario.DadosAutenticacao;
+import agendamento.med.api.domain.usuario.Usuario;
+import agendamento.med.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,13 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager manager;
+    @Autowired
+    private TokenService tokenService;
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
-
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
