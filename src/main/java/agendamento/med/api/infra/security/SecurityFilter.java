72f87582db0155c168f3d67dd8e92a13af5e6aa3
Danilo Pezo <danilo_97pem@hotmail.com>
2024-01-06 17:15:21 -0300

    Recuperamos o token

--- src/main/java/agendamento/med/api/infra/security/SecurityFilter.java
+++ src/main/java/agendamento/med/api/infra/security/SecurityFilter.java
@@ -16,4 +16,12 @@ public class SecurityFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         filterChain.doFilter(request, response);
     }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
 }
