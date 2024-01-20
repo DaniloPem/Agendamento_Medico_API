6ea1f9b1bc6aae78765eeb55d58e6fc613e77524
Danilo Pezo <danilo_97pem@hotmail.com>
2024-01-20 18:36:39 -0300

    Mostrando mensage ao cliente das excepcoes

--- src/main/java/agendamento/med/api/controller/ConsultaController.java
+++ src/main/java/agendamento/med/api/controller/ConsultaController.java
@@ -1,9 +1,11 @@
 package agendamento.med.api.controller;
 
+import agendamento.med.api.domain.consulta.AgendaDeConsultas;
 import agendamento.med.api.domain.consulta.DadosAgendamentoConsulta;
 import agendamento.med.api.domain.consulta.DadosDetalhamentoConsulta;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+    @Autowired
+    private AgendaDeConsultas agenda;
 
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        System.out.println(dados);
-        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+        var dto = agenda.agendar(dados);
+        return ResponseEntity.ok(dto);
     }
 
 }
--- src/main/java/agendamento/med/api/infra/exception/TratadorDeErros.java
+++ src/main/java/agendamento/med/api/infra/exception/TratadorDeErros.java
@@ -1,5 +1,6 @@
 package agendamento.med.api.infra.exception;
 
+import agendamento.med.api.domain.consulta.ValidacaoException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -21,6 +22,11 @@ public class TratadorDeErros {
         var erros = ex.getFieldErrors();
 
         return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity tratarErro400(ValidacaoException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
     }
 
     //código omitido
