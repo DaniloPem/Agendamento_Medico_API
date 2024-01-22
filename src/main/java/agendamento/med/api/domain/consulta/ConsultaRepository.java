db2a1b3c8042bb5a0348a16fab77b483d8493816
Danilo Pezo <danilo_97pem@hotmail.com>
2024-01-22 00:20:12 -0300

    Adicionamos migrations

--- src/main/java/agendamento/med/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/agendamento/med/api/domain/consulta/ConsultaRepository.java
@@ -1,9 +1,11 @@
 package agendamento.med.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
+@Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     boolean existsByMedico(Long idMedico, LocalDateTime data);
 
